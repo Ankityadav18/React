5c1f184585669777db50c85d68961513a12457d8
Ankityadav18 <140178073+Ankityadav18@users.noreply.github.com>
2025-02-19 09:30:49 +0530

    code

--- App.js
+++ App.js
@@ -4,7 +4,77 @@ import ReactDOM from "react-dom/client"
 
 
 
-//!JSX -HTML -like or XML -like syntax
-const jsx=<h1>This is react🎯 </h1>
+//!Header section
+const Heading=()=>{
+  return (
+   <div className="header">
+      <div className="logo-container">
+         <img  className="logo"    src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"></img>
+      </div>
+      <div className="nav-items">
+         <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Cart</li>
+         </ul>
+
+      </div>
+
+   </div>
+
+   )
+}
+
+
+//!
+const RestaurantCard=()=>{
+   return (
+      <div className="res-card">
+         <div>
+          
+         </div>
+      </div>
+   )
+}
+
+
+
+
+
+
+
+
+
+const Body=()=>{
+   return (
+      <div className="body">
+         <div>
+            Search
+         </div>
+         <div className="res-container">
+
+         </div>
+
+      </div>
+
+   )
+}
+
+
+
+
+
+ const App=()=>{
+  return (
+   <div className="app">
+      <Heading/>
+      <Body/>
+
+   </div>
+  )
+ }
+
+
 const root=ReactDOM.createRoot(document.getElementById('root'));
-root.render(jsx)
+root.render(<App/>)
